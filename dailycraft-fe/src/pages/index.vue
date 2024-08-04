85eabf2399c0ba1f540728ab3f0c180ab1c85cc7
Cara Hamilton <carahami13@gmail.com>
2024-08-03 18:13:47 -0700

    adds vue and rails app

---
+++ dailycraft-fe/src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <v-text-field label="1" style="max-width: 200px"></v-text-field>
+    <v-text-field label="2" style="max-width: 200px"></v-text-field>
+    <v-btn>
+      Submit
+    </v-btn>
+  </div>
+
+</template>
+
+<script setup>
+  //
+  
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Full viewport height */
+  gap: 20px;
+}
+
+</style>
